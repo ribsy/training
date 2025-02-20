218fd4ee54b5b1a1d2f570139e8d90d84f75c4ae
ribsy <rseiersen@hotmail.com>
2025-02-19 20:16:41 -0800

    Update training.py

--- training.py
+++ training.py
@@ -413,11 +413,12 @@ def play_burndown():
     col1, col2, col3, col4, col5, col6, col7 = st.columns(7, gap="small")
     with col1:
         st.write("RISK")
-        st.write("####")
+        st.write("###")
         st.write("OPENED")
         st.write("FIXED")
     with col2:
         st.write("PRIOR")
+        st.write("###")
         prior_open = st.number_input("", value=0, step=1, key = "prior_open")
         prior_fixed = st.number_input("", value=0, step=1, key = "prior_fixed")
     with col3:
