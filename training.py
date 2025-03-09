48b2c912ddb70b45966d1dd6867d893bcd77f2e4
ribsy <rseiersen@hotmail.com>
2025-03-09 03:17:14 -0700

    Update training.py

--- training.py
+++ training.py
@@ -735,6 +735,7 @@ def random_number_game_with_brier_score():
                 
                 st.write(f"Modified Brier Score: {score}")
                 st.write(f"Final Money: ${st.session_state.counter}")
+                st.write(f"Original Ball Location: ${st.session_state.initial_value}")
             else:
                 st.write(f"You must Play A New Game before you can Make A Bet.")
 
