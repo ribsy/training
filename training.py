bb4d998b475cd2c225ad98dd7037550d38d81e90
ribsy <rseiersen@hotmail.com>
2025-03-09 03:36:01 -0700

    Update training.py

--- training.py
+++ training.py
@@ -696,7 +696,7 @@ def random_number_game_with_brier_score():
         st.session_state.counter = 100
 
     counter_placeholder = st.empty()
-    counter_placeholder.write(f"Money: ${st.session_state.counter}")
+    #counter_placeholder.write(f"Money: ${st.session_state.counter}")
         
     if st.button("Play A New Game"):
         st.session_state.counter = 100
