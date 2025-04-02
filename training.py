73ac865292d1d84f713833e91bd19b1909f41092
ribsy <rseiersen@hotmail.com>
2025-04-02 13:54:29 -0700

    Update training.py

--- training.py
+++ training.py
@@ -719,11 +719,11 @@ def random_number_game_with_brier_score():
     #st.image("data/rev_bayes_pool.png")
     st.image("./data/rev_bayes_pool.png")
 
-    a = "<b>This is a calibration game.<b> It's goal is to help you understand accuracy, precision and its costs – while confronting some amount of irriducible uncertainty.<br><br>"
-    b = "First, you are learning to forecast <b>using ranges.</b> The closer your range is to the 'true position' of the white ball...<b>the bigger your reward.</b> (<i>Your range based forecasts will be scored.</i>) "
-    c = "Conversely, the farther away your range is from the ball, or the more spread out your ranges is (<i>beyond reason</i>)...<b>the bigger your penalties.</b> "
-    d = "Also, the more information you seek, <b>the more it costs you.</b> <ul>And that cost gets larger the more information you ask for.</ul> "
-    e = "At the end, you will make a <b>precise guess</b> about the balls location. That guess will also be scored – and <b>you will be penalized</b> if you are far off. "
+    a = "This is a calibration game. It's goal is to help you understand accuracy, precision and its costs – while confronting some amount of irriducible uncertainty.<br><br>"
+    b = "First, you are learning to forecast using ranges. The closer your range is to the 'true position' of the white ball...the bigger your reward. (<i>Your range based forecasts will be scored.</i>) "
+    c = "Conversely, the farther away your range is from the ball, or the more spread out your ranges is (<i>beyond reason</i>)...the bigger your penalties. "
+    d = "Also, the more information you seek, the more it costs you. And that cost gets larger the more information you ask for. "
+    e = "At the end, you will make a precise guess about the balls location. That guess will also be scored – and you will be penalized if you are far off. "
     f = "NOTE: you will win awards for being close – with a whoppig $100 for being both precise and accurate...meaning by being spot on!"
     bayes_msg = a + b + c + d + e + f
     markdown_box("HOW THE GAME WORKS!", bayes_msg)
