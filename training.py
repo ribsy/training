61c08a7c5ba430f332b4290ecc23139241330e2d
ribsy <rseiersen@hotmail.com>
2025-04-17 11:54:50 -0700

    Update training.py

--- training.py
+++ training.py
@@ -301,7 +301,8 @@ def forecast_elephant():
         st.session_state.score = 0  
         st.rerun()  # Rerun the script to reflect the changes
 
-    st.write("\n\n")    
+    st.write("\n\n\n")  
+        
     # Initialize money_value in session state
     if "money_value" not in st.session_state:
         st.session_state.money_value = 100
