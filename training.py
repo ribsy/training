038ee20b8d612c8808df09353253bd16b66b5b12
ribsy <rseiersen@hotmail.com>
2025-04-17 17:00:52 -0700

    Update training.py

--- training.py
+++ training.py
@@ -556,7 +556,8 @@ def play_burndown():
 
     st.write("\n")
 
-    if st.button("Transfer Values"):
+    if st.button("Transfer Values") & 'total_open' in session.state:
+        
         st.session_state.mone_open = st.session_state.total_open  # Transfer total_open to mone_open
         st.session_state.mone_fixed = st.session_state.total_fixed  # Transfer total_fixed to mone_fixed
         
