049e0cae145dae850006fa6c63db38b94568366e
ribsy <rseiersen@hotmail.com>
2025-05-08 08:09:10 -0700

    Update training.py

--- training.py
+++ training.py
@@ -1543,7 +1543,7 @@ def play_crq():
     user_input = st.text_input("AI Assisted Company Lookup")
     if st.button("Submit"):
       st.write("You entered:", user_input)
-        st.write("\n\n")
+        st.markdown("<br><br>")
 
     company_revenue = st.number_input("Enter company revenue:", value=0.0)
     market_capitalization = st.number_input("Enter market capitalization:", value=0.0)
