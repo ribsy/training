b9474852e9ccc328ac65247c8e342e3016eb6783
ribsy <rseiersen@hotmail.com>
2025-05-08 09:02:33 -0700

    Update training.py

--- training.py
+++ training.py
@@ -1566,10 +1566,11 @@ def play_crq():
     col1, col2 = st.columns([4,1])  # Adjust column ratios as needed
         
     with col1:
-      st.markdown("#######")
+      #st.markdown("#######")
       input_2 = st.text_input("Input 2", key="input_2")
     with col2:
-      st.markdown("####")
+      #st.markdown("####")
+      st.write("")
       if st.button("Process 2", key="button_2"):
         handle_input("input_2", input_2)
 
@@ -1577,10 +1578,10 @@ def play_crq():
     col1, col2 = st.columns([4,1])  # Adjust column ratios as needed
         
     with col1:
-      st.markdown("#######")
+      #st.markdown("#######")
       input_3 = st.text_input("Input 3", key="input_3")
     with col2:
-      st.markdown("####")
+      #st.markdown("####")
       if st.button("Process 3", key="button_3"):
         handle_input("input_3", input_3)
         
